8690bb4b536dbafabd12bb7042b1a6d2fd89e92d
Emily Crook <Emily.A.Crook96@gmail.com>
2021-10-27 11:26:54 -0500

    set up get routes for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,10 +2,34 @@ const router = require = require('express').Router();
 const { User } = require('../../models');
 
 // GET /api/users
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
 
 // GET /api/users/1
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
+});
 
 // POST /api/users
 router.post('/', (req, res) => {});
